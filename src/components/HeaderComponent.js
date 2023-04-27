e69b914c9d6ead3cf4d61c070c3bf34155a0bb4a
Mohammed Mustafa <mohammed.mustafatz@inxiteout.ai>
2023-04-27 17:46:20 +0530

    master

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Image, Navbar } from 'react-bootstrap'
-import { Bars3CenterLeftIcon,LanguageIcon, BellIcon, EnvelopeIcon} from '@heroicons/react/24/outline'
-import { Link, useNavigate } from 'react-router-dom'
+import { Bars3CenterLeftIcon,LanguageIcon, BellIcon, EnvelopeIcon, PencilSquareIcon,UserIcon,PowerIcon} from '@heroicons/react/24/outline'
+import { useNavigate } from 'react-router-dom'
 
 function HeaderComponent({onButtonClick,showsidebarbutton,isButtonClicked}) {
   const navigate=useNavigate();
@@ -33,34 +33,90 @@ function HeaderComponent({onButtonClick,showsidebarbutton,isButtonClicked}) {
               )}
             </div>
               <div className='d-flex gap-2'>
-              <Link className='btn btn-light'>
-                <LanguageIcon className="text-black" style={{height:"18px",width:"18px"}}/>
-               </Link>
-               <Link className='btn btn-light'>
-                <BellIcon className="text-black" style={{height:"18px",width:"18px"}}/>
-               </Link>
+              <div className="dropdown">
+                <button className="btn btn-light" aria-expanded="false" data-bs-toggle="dropdown" type="button">  
+                  <LanguageIcon className="text-black" style={{height:"18px",width:"18px"}}/>
+                </button>
+                <div className="dropdown-menu dropdown-menu-start dropdown-menu-lg-end border-0 shadow-lg mt-2 p-0">
+                  <button className="dropdown-item small btn-light my-btn border-bottom py-3" onClick={navigationlinks}>
+                    English (UK)
+                  </button>
+                  <button className="dropdown-item small btn-light my-btn border-bottom py-3" onClick={navigationlinks}>
+                    French (France)
+                  </button>
+                  <button className="dropdown-item small btn-light my-btn py-3" onClick={navigationlinks}>
+                    Chinese (China)
+                  </button>
+                </div>
+              </div>
+               <div className="dropdown">
+                <button className="btn btn-light" aria-expanded="false" data-bs-toggle="dropdown" type="button">  
+                  <BellIcon className="text-black" style={{height:"18px",width:"18px"}}/>
+                </button>
+                <div className="dropdown-menu dropdown-menu-end border-0 shadow-lg mt-2 p-0" style={{width:"250px"}}>
+                  <p className="dropdown-item mb-0 text-start border-bottom py-3 disabled text-black" onClick={navigationlinks}>
+                    Notifications
+                  </p>
+                  <button className="dropdown-item btn-light small d-flex justify-content-between border-bottom py-3" onClick={navigationlinks}>
+                    <p className='mb-0 text-wrap'>John Wick commented on your post.</p>
+                    <small className="text-muted float-end">12:57 PM</small>
+                  </button>
+                  <button className="dropdown-item btn-light small d-flex justify-content-between border-bottom py-3" onClick={navigationlinks}>
+                    <p className='mb-0 text-wrap'>John Wick commented on your post.</p>
+                    <small className="text-muted float-end">12:57 PM</small>
+                  </button>
+                  <button className="dropdown-item btn-light small d-flex justify-content-between border-bottom py-3" onClick={navigationlinks}>
+                    <p className='mb-0 text-wrap'>John Wick commented on your post.</p>
+                    <small className="text-muted float-end">12:57 PM</small>
+                  </button>
+                  <button className="dropdown-item btn-light my-btn text-center py-3" onClick={navigationlinks}>
+                    <p className='my-0 text-primary'>View All</p>
+                  </button>
+                </div>
+              </div>
                <div className="dropdown">
                 <button className="btn btn-light" aria-expanded="false" data-bs-toggle="dropdown" type="button">  
                   <EnvelopeIcon className="text-black" style={{height:"18px",width:"18px"}}/>
                 </button>
-                <div className="dropdown-menu dropdown-menu-end mt-2">
-                  <button className="dropdown-item btn-light d-flex justify-content-between" onClick={navigationlinks} style={{width:"250px"}}>
-                    <p className='mb-0 text-wrap' style={{width:"150px"}}>John Wick commented on your post.</p>
+                <div className="dropdown-menu dropdown-menu-end border-0 shadow-lg mt-2 p-0" style={{width:"250px"}}>
+                  <p className="dropdown-item mb-0 text-start border-bottom py-3 disabled text-black" onClick={navigationlinks}>
+                    Messages
+                  </p>
+                  <button className="dropdown-item btn-light small d-flex justify-content-between border-bottom py-3" onClick={navigationlinks}>
+                    <p className='mb-0 text-wrap'>John Wick commented on your post.</p>
                     <small className="text-muted float-end">12:57 PM</small>
                   </button>
-                  <button className="dropdown-item btn-light" onClick={navigationlinks}>View Profile</button>
-                  <button className="dropdown-item btn-light" onClick={handleLogout}>Sign Out</button>
+                  <button className="dropdown-item btn-light small d-flex justify-content-between border-bottom py-3" onClick={navigationlinks}>
+                    <p className='mb-0 text-wrap'>John Wick commented on your post.</p>
+                    <small className="text-muted float-end">12:57 PM</small>
+                  </button>
+                  <button className="dropdown-item btn-light small d-flex justify-content-between border-bottom py-3" onClick={navigationlinks}>
+                    <p className='mb-0 text-wrap'>John Wick commented on your post.</p>
+                    <small className="text-muted float-end">12:57 PM</small>
+                  </button>
+                  <button className="dropdown-item btn-light my-btn text-center py-3" onClick={navigationlinks}>
+                    <p className='my-0 text-primary'>View All</p>
+                  </button>
                 </div>
               </div>
               <div className="dropdown">
-                <button className="btn text-center" aria-expanded="false" data-bs-toggle="dropdown" type="button">
+                <button className="btn text-center border-0" aria-expanded="false" data-bs-toggle="dropdown" type="button">
                   <Image src='/avatar.jpg' className='rounded-5 p-0 me-0 me-lg-1' style={{height:"22px", width:"22px"}}/>
                   <span className='d-none d-lg-inline fw-bold'>Mustafa</span>
                 </button>
-                <div className="dropdown-menu dropdown-menu-end mt-2">
-                  <button className="dropdown-item btn-light" onClick={navigationlinks}>Edit Profile</button>
-                  <button className="dropdown-item btn-light" onClick={navigationlinks}>View Profile</button>
-                  <button className="dropdown-item btn-light" onClick={handleLogout}>Sign Out</button>
+                <div className="dropdown-menu dropdown-menu-end shadow-lg border-0 p-0 mt-2">
+                <button className="dropdown-item small btn-light py-3" onClick={navigationlinks}>
+                  <PencilSquareIcon className="text-black me-2" style={{height:"20px",width:"20px"}}/>
+                  Edit Profile
+                </button>
+                <button className="dropdown-item small btn-light py-3" onClick={navigationlinks}>
+                  <UserIcon className="text-black me-2" style={{height:"20px",width:"20px"}}/>
+                  View Profile
+                </button>
+                <button className="dropdown-item small btn-light py-3" onClick={handleLogout}>
+                  <PowerIcon className="text-black me-2" style={{height:"20px",width:"20px"}}/>
+                  Sign Out
+                </button>
                 </div>
               </div>
             </div>
